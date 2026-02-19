3e08b28ce2e8a5be42b9ef77be1073565a4e2710
lin-coln001 <clydewachira@gmail.com>
2026-02-19 10:50:10 +0300

    first commit

---
+++ app1/views.py
@@ -0,0 +1,16 @@
+from urllib import request
+
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def portfolio_details(request):
+    return render(request, 'portfolio-details.html')
+
+def service_details(request):
+    return render(request, 'service-details.html')
+
+def starter_page(request):
+    return render(request, 'starter-page.html')
